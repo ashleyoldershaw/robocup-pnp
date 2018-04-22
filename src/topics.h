0295ab4efff927d2059157b8c7d242ed35d7b2c3
Luca Iocchi <iocchi@dis.uniroma1.it>
2018-04-22 15:08:17 +0200

    ROS names from pnp_ros

--- src/topics.h
+++ src/topics.h
@@ -3,6 +3,8 @@
 #ifndef __TOPICS__
 #define __TOPICS__
 
+#include <pnp_ros/names.h>  // PNP topics
+
 #define TOPIC_ROBOT_LOCATION "amcl_pose"
 #define TOPIC_LASER_OBSMAP "laser_obstacle_map"
 
@@ -12,13 +14,6 @@
 #define TOPIC_FOLLOWPERSON "follow_person"
 #define TOPIC_SAY "say"
 #define TOPIC_STAGE_SAY "stage_say"
-
-#define TOPIC_PLANTOEXEC "planToExec"
-#define TOPIC_PNPACTIVEPLACES "pnp/currentActivePlaces"
-#define TOPIC_PNPCONDITION "PNPConditionEvent"
-#define TOPIC_PNPACTIONCMD "PNPActionCmd"
-
-#define PARAM_PNPCONDITIONBUFFER "PNPconditionsBuffer/"
 
 #define TOPIC_RCOMMESSAGE "/RCOMMessage"
 
