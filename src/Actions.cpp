e24fad1c9457883ee7b8133a2a2ae9dd672c7533
Ashley <ash.oldershaw@gmail.com>
2018-07-26 11:02:23 +0200

    deleting demo path code

--- src/Actions.cpp
+++ src/Actions.cpp
@@ -253,17 +253,6 @@ void RCHPNPActionServer::MODIM_init() {
     sendMODIM("im.display.remove_buttons()");
     
     sendMODIM("im.setProfile(['*', '*', 'en', '*'])");
-
-    char* demo_p = getenv ("DEMO_PATH");
-    std::string demo_path = "/home/ubuntu/demos/cocktail_party_demo/";
-    if (demo_p!=NULL){
-       demo_path = std::string(demo_p);
-       printf ("DEMO_PATH defined. Using %s", demo_path.c_str());
-    } else
-       printf ("DEMO_PATH not defined. Using %s", demo_path.c_str());
-
-
-    sendMODIM("im.setPath('"+demo_path+"')");
     
     sendMODIM_text("Welcome");
 }
